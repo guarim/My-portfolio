3fb52d3daa0416f160c7dec7baf6e06524369864
MOHAMED GUARIM <84085689+guarim@users.noreply.github.com>
2023-06-30 13:02:42 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,18 +44,18 @@ function scrollActive(){
 /*===== SCROLL REVEAL ANIMATION =====*/
 const sr = ScrollReveal({
     origin: 'top',
-    distance: '180px',
-    duration: 2000,
+    distance: '280px',
+    duration: 3000,
     reset: true
 })
 
 /*SCROLL HOME*/
 sr.reveal('.home__title', {})
-sr.reveal('.home__scroll', {delay: 200})
-sr.reveal('.home__img', {origin:'right', delay: 400})
+sr.reveal('.home__scroll', {delay: 300})
+sr.reveal('.home__img', {origin:'right', delay: 500})
 
 /*SCROLL ABOUT*/
-sr.reveal('.about__img', {delay: 500})
+sr.reveal('.about__img', {delay: 600})
 sr.reveal('.about__subtitle', {delay: 300})
 sr.reveal('.about__profession', {delay: 400})
 sr.reveal('.about__text', {delay: 500})
